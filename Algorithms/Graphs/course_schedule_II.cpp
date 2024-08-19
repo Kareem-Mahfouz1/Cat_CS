e11534efee54002875ae849a3b2bd91646fd08bd
kareem mahfouz <samkrym123@gmail.com>
2024-08-19 18:49:36 +0300

    asad

---
+++ Algorithms/Graphs/course_schedule_II.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+using namespace std;
+class Solution
+{
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>> &preq)
+    {
+        vector<int> adj[numCourses];
+        queue<int> q;
+        vector<int> completed;
+        for (int i = 0; i < preq.size(); i++)
+        {
+            int to = preq[i][0];
+            int from = preq[i][1];
+            adj[from].push_back(to);
+        }
+        vector<int> degree(numCourses, 0);
+        for (int i = 0; i < numCourses; i++)
+        {
+            for (auto it : adj[i])
+                degree[it]++;
+        }
+        for (int i = 0; i < numCourses; i++)
+            if (degree[i] == 0)
+                q.push(i);
+        while (!q.empty())
+        {
+            int cur = q.front();
+            q.pop();
+            completed.push_back(cur);
+            for (auto neighbor : adj[cur])
+                if (--degree[neighbor] == 0)
+                    q.push(neighbor);
+        }
+        if (completed.size() == numCourses)
+            return completed;
+        return {};
+    }
+};
